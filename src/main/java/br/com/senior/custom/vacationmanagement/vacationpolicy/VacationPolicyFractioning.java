61e1ef6119e3697a61c2903e81a186bf2b975c0d
gabriel.kresin <gabriel.kresin@senior.com.br>
2019-08-20 09:42:52 -0300

    commit número 15.7

--- src/main/java/br/com/senior/custom/vacationmanagement/vacationpolicy/VacationPolicyFractioning.java
+++ src/main/java/br/com/senior/custom/vacationmanagement/vacationpolicy/VacationPolicyFractioning.java
@@ -1,5 +1,8 @@
 package br.com.senior.custom.vacationmanagement.vacationpolicy;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VacationPolicyFractioning {
 
     /**
